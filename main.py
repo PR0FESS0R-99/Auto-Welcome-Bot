0199f44cd9d33a47f83756ceca164832ce892f36
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-10-08 20:38:57 +0530

    💡

--- main.py
+++ main.py
@@ -74,13 +74,6 @@ async def admin(bot: Pr0fess0r_99, update):
 @Pr0fess0r_99.on_message(filters.new_chat_members)
 async def auto_welcome(bot: Pr0fess0r_99, msg: Message):
     # from PR0FESS0R-99 import ID-Bot
-    first = msg.from_user.first_name
-    last = msg.from_user.last_name
-    mention = msg.from_user.mention
-    username = msg.from_user.username
-    id = msg.from_user.id
-    group_name = msg.chat.title
-    group_username = msg.chat.username
     welcome_text = f"👋Hey {mention}, Welcome To {group_name}\n\n Developed By @Mo_Tech_YT"
     welcome_msg = os.environ.get(f"WELCOME_TEXT", welcome_text)
     print("Welcome Message Activate")
